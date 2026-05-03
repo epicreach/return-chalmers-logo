2d8977b8d563784beed9e9166e27e20c1967afe8
epicreach <luqas.lundahl4817@gmail.com>
2026-05-03 22:15:17 +0200

    added button color and logo img

---
+++ returnchalmers.js
@@ -0,0 +1,6 @@
+// Color Button: #006c5c
+
+let loginbtn = document.getElementById("link-saml");
+loginbtn.style.backgroundColor = "#006c5c";
+
+document.querySelector("header img").src = "https://i.imgur.com/CjzH3dN.png";
