c98a8ab8e47025aa1e75bf5ae823e4fb1ae95899
Luqas Lundahl <luqas.lundahl4817@gmail.com>
2026-05-20 11:44:21 +0200

    added canvas and chalmers.se

--- returnchalmers.js
+++ returnchalmers.js
@@ -1,6 +1,39 @@
 // Color Button: #006c5c
 
-let loginbtn = document.getElementById("link-saml");
-loginbtn.style.backgroundColor = "#006c5c";
 
-document.querySelector("header img").src = "https://i.imgur.com/CjzH3dN.png";
+//Canvas login
+const loginbtn = document.getElementById("link-saml");
+if (loginbtn) {
+  loginbtn.style.backgroundColor = "#006c5c";
+  const img = document.querySelector('header img');
+  img.src = "https://i.imgur.com/CjzH3dN.png";
+}
+
+
+//Chalmers.se
+function swapLogo() {
+  const svg = document.querySelector('a[aria-label="To frontpage"] svg, a[aria-label="Till startsidan"] svg');
+  if (svg) {
+    const img = document.createElement('img');
+    img.src = "https://i.imgur.com/CjzH3dN.png";
+    img.alt = 'Logo';
+    img.style.height = '43px';
+    img.style.width = '161px';
+    svg.parentNode.replaceChild(img, svg);
+  }
+}
+
+swapLogo();
+
+const observer = new MutationObserver(() => {
+  const svg = document.querySelector('a[aria-label="To frontpage"] svg, a[aria-label="Till startsidan"] svg');
+  if (svg) swapLogo();
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
+
+//Microsoft login
+const logo = document.getElementById('bannerLogo');
+if (logo) {
+  logo.src = "https://i.imgur.com/CjzH3dN.png";
+}
